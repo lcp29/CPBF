96ca0c4f4672a55f57c60aa7a07cd4b0ca7c46e3
lcp29 <2504274857@qq.com>
2016-08-21 09:14:31 +0800

    Create CPBF.cpp
    Add the main program.
    添加主程序。

---
+++ CPBF.cpp
@@ -0,0 +1,16 @@
+//
+// CPBF - CP's Brainfuck
+//
+// Version 0.01
+//
+// Copyright (c) 2016 lcp29
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
